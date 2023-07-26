244dbe3dbc7fcd09ed797bc11852874b4fcb21b6
aidanlw505 <aidanlw505@gmail.com>
2023-07-26 00:30:16 -0600

    fixed api and updated commands

--- test_busydrones.lua
+++ test_busydrones.lua
@@ -1,5 +1,5 @@
 -- Load the busydrones library (assuming it's in the same directory)
-os.loadAPI("busydrones.lua")
+os.loadAPI("computercraftfiles/busydrones.lua")
 
 -- Function to display the current direction
 local function displayCurrentDirection()
