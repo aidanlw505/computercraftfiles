9159cf9e578c475ccc0cbe95fdd4da67be1d53b6
aidanlw505 <aidanlw505@gmail.com>
2023-07-26 00:14:20 -0600

    writing direction stuff

--- busydrones.lua
+++ busydrones.lua
@@ -1,14 +1,55 @@
 ---@diagnostic disable: unused-function, unused-local
 local directions = {
-    NORTH = "0", -- Positive Z
-    EAST = "1", -- Positive X
-    SOUTH = "2", -- Negative Z
-    West = "3" -- Negative X
+    NORTH = 0, -- Positive Z
+    EAST = 1, -- Positive X
+    SOUTH = 2, -- Negative Z
+    WEST = 3 -- Negative X
 }
 
-local function setDirection() end
+local function setDirection(direction)
+    local currentDirection = getDirection()
 
-local function getDirection() end
+    local ddirection = (direction - currentDirection) % 4
+
+    if ddirection == 0 then
+        return
+    elseif ddirection == 1 then
+        turtle.turnLeft()
+        return
+    elseif ddirection == 2 then
+        turtle.turnRight()
+        return
+    else
+        turtle.turnRight()
+        turtle.turnRight()
+        return
+    end
+end
+
+local function getDirection()
+    -- turn right until there is no block in front of you,
+    -- if you are surrounded by blocks, break the one in front
+    for i = 1, 5, 1 do if turtle.detect() then turtle.turnRight() end end
+
+    local oldX, _, oldZ = gps.locate()
+    turtle.forward()
+    local newX, _, newZ = gps.locate()
+    turtle.back()
+
+    local dx = newX - oldX
+    local dz = newZ - oldZ
+
+    if dx > 0 then
+        return directions.NORTH
+    elseif dx < 0 then
+        return directions.SOUTH
+    elseif dz > 0 then
+        return directions.EAST
+    else
+        return directions.WEST
+    end
+end
+
 local function mineArea(x, y, z, dir, w, d, h) end
 
 local function mineForward() end
---
+++ test_busydrones.lua
@@ -0,0 +1,32 @@
+-- Load the busydrones library (assuming it's in the same directory)
+os.loadAPI("busydrones.lua")
+
+-- Function to display the current direction
+local function displayCurrentDirection()
+    local currentDir = busydrones.getDirection()
+    print("Current Direction: " .. currentDir)
+end
+
+-- Test setDirection and getDirection functions
+function testDirectionFunctions()
+    displayCurrentDirection()
+
+    print("Setting direction to EAST...")
+    busydrones.setDirection("EAST")
+    displayCurrentDirection()
+
+    print("Setting direction to SOUTH...")
+    busydrones.setDirection("SOUTH")
+    displayCurrentDirection()
+
+    print("Setting direction to WEST...")
+    busydrones.setDirection("WEST")
+    displayCurrentDirection()
+
+    print("Setting direction to NORTH...")
+    busydrones.setDirection("NORTH")
+    displayCurrentDirection()
+end
+
+-- Run the test
+testDirectionFunctions()
